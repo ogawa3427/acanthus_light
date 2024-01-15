a110363b13cf833210f943a4b374a7d258e18f18
Ogawa <135859092+ogawa3427@users.noreply.github.com>
2024-01-15 23:49:00 +0900

    ごめんなさい

--- al/pages/api/abe.js
+++ al/pages/api/abe.js
@@ -1,33 +1,18 @@
-import puppeteer from 'puppeteer-core';
-import chrome from 'chrome-aws-lambda';
+import puppeteer from 'puppeteer';
 
 export default async function handler(req, res) {
-  let browser = null;
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://kurisyushien.org/man', { waitUntil: 'networkidle0' });
+  const bodyHandle = await page.$('body');
 
-  try {
-    browser = await puppeteer.launch({
-      args: chrome.args,
-      executablePath: await chrome.executablePath,
-      headless: chrome.headless,
-    });
+  if (bodyHandle) {
+    const html = await page.evaluate(body => body.innerText, bodyHandle);
+    await bodyHandle.dispose();
+    await browser.close();
 
-    const page = await browser.newPage();
-    await page.goto('https://kurisyushien.org/man', { waitUntil: 'networkidle0' });
-    const bodyHandle = await page.$('body');
-
-    if (bodyHandle) {
-      const html = await page.evaluate(body => body.innerText, bodyHandle);
-      await bodyHandle.dispose();
-      res.status(200).json({ text: html });
-    } else {
-      res.status(500).json({ error: 'Body element not found' });
-    }
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  } finally {
-    if (browser !== null) {
-      await browser.close();
-    }
+    res.status(200).json({ text: html });
+  } else {
+    res.status(500).json({ error: 'Body element not found' });
   }
 }
