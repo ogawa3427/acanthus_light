5967a2f49d6d282351cd8a37fdde91df432ec046
Ogawa <135859092+ogawa3427@users.noreply.github.com>
2024-01-15 23:54:58 +0900

    aaaa

--- al/pages/index.js
+++ al/pages/index.js
@@ -1,18 +1,12 @@
 import React, { useEffect, useState } from 'react';
 
 function AbePage() {
-  const [text, setText] = useState('');
 
-  useEffect(() => {
-    fetch('http://localhost:3000/api/abe')
-      .then(response => response.json())
-      .then(data => setText(data.text));
-  }, []);
+
 
   return (
     <div>
       <h1>スクレイピングのテストです</h1>
-      <pre>{text}</pre>
     </div>
   );
 }
