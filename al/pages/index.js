429cb17c635ad8b0f7a490341e4261af369d7d3d
Ogawa <135859092+ogawa3427@users.noreply.github.com>
2024-01-16 22:14:59 +0900

    URL切り替え

--- al/pages/index.js
+++ al/pages/index.js
@@ -1,12 +1,24 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function AbePage() {
+  const [text, setText] = useState('');
+  const apiUrl = process.env.NEXT_PUBLIC_API_URL + '/api/abe';
 
-
+  useEffect(() => {
+    axios.get('http://localhost:3000/api/abe')
+      .then(response => {
+        setText(response.data.text);
+      })
+      .catch(error => {
+        console.error('There was an error!', error);
+      });
+  }, []);
 
   return (
     <div>
       <h1>スクレイピングのテストです</h1>
+      <p>{text}</p>
     </div>
   );
 }
